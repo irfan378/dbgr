ec88e0f0be0c8683689a0afd357c102606fd5817
irfan378 <irfanbhat3@proton.me>
2024-07-11 02:07:29 -0400

    added memory read and write

--- src/main.cpp
+++ src/main.cpp
@@ -30,11 +30,18 @@ bool is_prefix(const std::string& s,const std::string& of){
 }
 
 void debugger::continue_execution(){
-	ptrace(PTRACE_CONT,m_pid,nullptr,nullptr);
 
-	int wait_status;
-	auto options=0;
-	waitpid(m_pid,&wait_status,options);
+	step_over_breakpoint();
+	ptrace(PTRACE_CONT,m_pid,nullptr,nullptr);
+	wait_for_signal();
+}
+
+uint64_t debugger::read_memory(uint64_t address){
+	return ptrace(PTRACE_PEEKDATA,m_pid,address,nullptr);
+}
+
+void debugger::write_memory(uint64_t address,uint64_t value){
+	ptrace(PTRACE_POKEDATA,m_pid,address,value);
 }
 
 void debugger::dump_registers(){
@@ -43,7 +50,38 @@ void debugger::dump_registers(){
 	}
 }
 
+uint64_t debugger::get_pc(){
+	return get_register_value(m_pid,reg::rip);
+}
 
+void debugger::set_pc(uint64_t pc){
+	set_register_value(m_pid,reg::rip,pc);
+}
+
+void debugger::step_over_breakpoint(){
+	auto possible_breakpoint_location=get_pc()-1;
+
+	if(m_breakpoints.count(possible_breakpoint_location)){
+		auto& bp=m_breakpoints[possible_breakpoint_location];
+
+		if(bp.is_enabled()){
+			auto previous_instruction_address=possible_breakpoint_location;
+			set_pc(previous_instruction_address);
+
+			bp.disable();
+			ptrace(PTRACE_SINGLESTEP,m_pid,nullptr,nullptr);
+			wait_for_signal();
+			bp.enable();
+		}
+	}
+}
+
+
+void debugger::wait_for_signal(){
+	int wait_status;
+	auto options=0;
+	waitpid(m_pid,&wait_status,options);
+}
 void debugger::handle_command(const std::string& line){
 	auto args=split(line,' ');
 	auto command=args[0];
@@ -63,7 +101,17 @@ void debugger::handle_command(const std::string& line){
 			std::string val {args[3],2};
 			set_register_value(m_pid,get_register_from_name(args[2]),std::stol(val,0,16));
 		}
-	}else{
+	}else if(is_prefix(command,"memory")){
+		std::string addr{args[2],2};
+		  if (is_prefix(args[1], "read")) {
+            		std::cout << std::hex << read_memory(std::stol(addr, 0, 16)) << std::endl;
+        	  }
+		  if(is_prefix(args[1],"write")){
+			std::string val{args[3],2};
+			write_memory(std::stol(addr,0,16),std::stol(val,0,16));
+		  }
+	}
+	else{
 		std::cerr<<"Unknown Command\n";
 	}
 
