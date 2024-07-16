4aa60620624f7f45b3f77d3d93254439d4b5d363
irfan378 <irfanbhat3@proton.me>
2024-07-16 01:44:58 -0400

    added support for position independant executables

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,20 @@
 #include<sys/ptrace.h>
 #include<sys/wait.h>
 #include<unistd.h>
+#include<fstream>
 #include<sstream>
 #include<iostream>
 #include <iomanip>
 #include<cstring>
+
 #include "linenoise.h"
 #include "debugger.hpp"
 #include "registers.hpp"
+
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
 #include<sys/personality.h>
 using namespace minidbg;
 
@@ -59,15 +66,10 @@ void debugger::set_pc(uint64_t pc){
 }
 
 void debugger::step_over_breakpoint(){
-	auto possible_breakpoint_location=get_pc()-1;
-
-	if(m_breakpoints.count(possible_breakpoint_location)){
-		auto& bp=m_breakpoints[possible_breakpoint_location];
-
+	
+	if(m_breakpoints.count(get_pc())){
+		auto& bp=m_breakpoints[get_pc()];
 		if(bp.is_enabled()){
-			auto previous_instruction_address=possible_breakpoint_location;
-			set_pc(previous_instruction_address);
-
 			bp.disable();
 			ptrace(PTRACE_SINGLESTEP,m_pid,nullptr,nullptr);
 			wait_for_signal();
@@ -81,6 +83,42 @@ void debugger::wait_for_signal(){
 	int wait_status;
 	auto options=0;
 	waitpid(m_pid,&wait_status,options);
+	
+	auto siginfo=get_signal_info();
+
+	switch(siginfo.si_signo){
+	case SIGTRAP:
+		handle_sigtrap(siginfo);
+		break;
+	case SIGSEGV:
+		std::cout<< "Segfault. Reason: "<<siginfo.si_code<<std::endl;
+		break;
+	default:
+		std::cout<< "Got signal "<<strsignal(siginfo.si_signo) << std::endl;
+	}
+}
+
+void debugger::handle_sigtrap(siginfo_t info){
+	switch(info.si_code){
+
+		case SI_KERNEL:
+		case TRAP_BRKPT:
+		{
+			set_pc(get_pc()-1);
+			std::cout << "Hit breakpoint at address 0x" << std::hex << get_pc() <<std::endl;
+			auto offset_pc=offset_load_address(get_pc());	
+
+			auto line_entry=get_line_entry_from_pc(offset_pc);
+			print_source(line_entry->file->path,line_entry->line);
+			return;
+			
+		}
+		case TRAP_TRACE:
+			return;
+		default:
+			std::cout << "Unknown SIGTRAP code " << info.si_code << std::endl;
+			return;
+	}
 }
 void debugger::handle_command(const std::string& line){
 	auto args=split(line,' ');
@@ -151,30 +189,80 @@ void debugger::handle_command(const std::string& line){
 }
 
 void debugger::print_memory_mappings(){
- // Prepare the command to execute
     char command[100];
     snprintf(command, sizeof(command), "/bin/cat /proc/%d/maps",m_pid);
 
-    // Open a pipe to the command
     FILE *fp = popen(command, "r");
     if (fp == nullptr) {
         std::cerr << "Failed to execute command: " << strerror(errno) << std::endl;
         return;
     }
 
-    // Read and print the output
     char buffer[256];
     while (fgets(buffer, sizeof(buffer), fp) != nullptr) {
         std::cout << buffer;
     }
 
-    // Close the pipe
     pclose(fp);
+}
+
+void debugger::intialise_load_address(){
+	
+	if(m_elf.get_hdr().type==elf::et::dyn){
+		
+		std::ifstream map("/proc/"+ std::to_string(m_pid) + "/maps");
+
+		std::string addr;
+		std::getline(map,addr,'-');
+
+		m_load_address=std::stoi(addr,0,16);
+	}
+}
+
+uint64_t debugger::offset_load_address(uint64_t addr){
+	return addr-m_load_address;
+}
+
+
+siginfo_t debugger::get_signal_info(){
+	siginfo_t info;
+	ptrace(PTRACE_GETSIGINFO,m_pid,nullptr,&info);
+	return info;
+}
+
+void debugger::print_source(const std::string& file_name,unsigned line,unsigned n_lines_context){
+	std::ifstream file {file_name};
+
+	auto start_line=line<=n_lines_context?1:line-n_lines_context;
+	auto end_line=line+n_lines_context+(line < n_lines_context? n_lines_context-line:0)+1;
+
+	char c{};
+	auto current_line=1u;
+
+	while(current_line!=start_line && file.get(c)){
+		if(c=='\n'){
+			++current_line;
+		}
+	}
+
+	std::cout << (current_line==line? "> ": " ");
+
+	while(current_line<=end_line && file.get(c)){
+		std::cout << c;
+		if(c=='\n'){
+			++current_line;
+			std::cout<< (current_line==line?"> ":" ");
+		}
+	}
+
+	std::cout<<std::endl;
 }
 void debugger::run(){
 	int wait_status;
 	auto options=0;
 	waitpid(m_pid,&wait_status,options);
+	
+	intialise_load_address();
 
 	char *line=nullptr;
 	while((line=linenoise("dbgr> "))!=nullptr){
@@ -188,6 +276,38 @@ void debugger::run(){
 	}	
 }
 
+dwarf::die debugger::get_function_from_pc(uint64_t pc){
+	for(auto& cu:m_dwarf.compilation_units()){
+		if(die_pc_range(cu.root()).contains(pc)){
+			for(const auto& die:cu.root()){
+				if(die.tag==dwarf::DW_TAG::subprogram){
+					if(die_pc_range(die).contains(pc)){
+						return die;
+					}
+				}
+			}
+		}
+	}
+
+	throw std::out_of_range{"Cannot find function"};
+}
+
+dwarf::line_table::iterator debugger::get_line_entry_from_pc(uint64_t pc){
+	for (auto& cu:m_dwarf.compilation_units()){
+		if(die_pc_range(cu.root()).contains(pc)){
+			auto &lt=cu.get_line_table();
+			auto it=lt.find_address(pc);
+
+			if(it==lt.end()){
+				throw std::out_of_range{"Cannot find line entry"};
+
+			}else{
+				return it;
+			}
+		}
+	}
+	throw std::out_of_range{"Cannot find line entry"};
+}
 void debugger::set_breakpoint_at_address(std::intptr_t addr){
 	std::cout<<"Set breakpoint  at address 0x"<< std::hex << addr << std::endl;
 	breakpoint bp{m_pid,addr};
